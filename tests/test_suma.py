7bc22e787108925a60be02b164b2efd89dbe59e2
carlosdelgadillo10 <carlos.delgadillo102003@gmail.com>
2024-07-02 15:21:06 -0600

    s

---
+++ tests/test_suma.py
@@ -0,0 +1,12 @@
+
+import pytest
+from fastapi.testclient import TestClient
+from app.suma import app
+
+client = TestClient(app)
+
+def test_sumar():
+    response = client.post("/sumar", json={"num1": 10, "num2": 5})
+    assert response.status_code == 200
+    assert response.json() == 10
+
